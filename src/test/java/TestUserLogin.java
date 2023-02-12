0d48051b2479e0f854924eae94abf6ddedff8362
Evgeniya Molodtsova <molodtsova_95@mail.ru>
2023-02-13 00:05:48 +0100

    add test for login

---
+++ src/test/java/TestUserLogin.java
@@ -0,0 +1,59 @@
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.response.ValidatableResponse;
+import org.example.User;
+import org.example.UserClient;
+import org.example.UserGeneration;
+import org.example.UserLogin;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestUserLogin {
+    private final UserGeneration userGeneration = new UserGeneration();
+    private String token;
+    private final UserClient userClient = new UserClient();
+    private User user;
+
+    @Before
+    public void setUp() {
+        user = userGeneration.getDefault();
+        ValidatableResponse createUser = userClient.create(user);
+        token = createUser.extract().path("accessToken");
+    }
+
+    @After
+    public void cleanUp() {
+        if (token != null) {
+            userClient.delete(token);
+        }
+    }
+
+    @Test
+    @DisplayName("логин с валидными логином и паролем")
+    public void userLogin(){
+        ValidatableResponse login = userClient.login(UserLogin.from(user));
+
+        Assert.assertEquals(200, login.extract().statusCode());
+    }
+
+    @Test
+    @DisplayName("невозможен логин несуществующего пользователя")
+    public void notExistingUser(){
+        User notExisting = userGeneration.getDefault();
+
+        ValidatableResponse login = userClient.login(UserLogin.from(notExisting));
+
+        Assert.assertEquals(401, login.extract().statusCode());
+    }
+
+    @Test
+    @DisplayName("невозможен логин с валидным email и неверным паролем")
+    public void userLoginWithIncorrectPassword(){
+        String email = user.getEmail();
+
+        ValidatableResponse login = userClient.login(UserLogin.create(email, "eiu8whjk"));
+
+        Assert.assertEquals(401, login.extract().statusCode());
+    }
+}
